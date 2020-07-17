babb9c96205326ed1cd6f0ff769776cee7cbae65
Marcus Paulo Augusto <mpsmvsjrp@hotmail.com>
2020-07-17 01:19:00 -0300

    Starting a new implements

---
+++ MongoDBWrapper/MongoDBWrapper/Helpers/Builders.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MongoDBWrapper.Helpers
+{
+    public class Builders
+    {
+        public static FilterDefinition<BsonDocument> FilterEq(string field, string value)
+        {
+            return Builders<BsonDocument>.Filter.Eq(field, value);
+        }
+
+        public static FilterDefinition<BsonDocument> FilterEq<T>(string field, T value)
+        {
+            return Builders<BsonDocument>.Filter.Eq(field, value);
+        }
+
+        public static FilterDefinition<BsonDocument> FilterEq(string field, ObjectId id)
+        {
+            return Builders<BsonDocument>.Filter.Eq(field, id);
+        }
+
+        public static UpdateDefinition<BsonDocument> Update<T>(string field, T value)
+        {
+            return Builders<BsonDocument>.Update.Set(field, value);
+        }
+    }
+}
---
+++ MongoDBWrapper/MongoDBWrapper/Helpers/MongDBHelper.cs
@@ -0,0 +1,62 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MongoDBWrapper.Helpers
+{
+    public class MongDBHelper : IDisposable
+    {
+        protected static IMongoClient _client;
+        protected static IMongoDatabase _database;
+
+        public MongDBHelper(string connectionString, string dbName)
+        {
+            _client = new MongoClient(connectionString);
+            _database = _client.GetDatabase(dbName);
+        }
+
+        public long? SelectCount(string collectionName)
+        {
+            try
+            {
+                var collection = _database.GetCollection<BsonDocument>(collectionName);
+                return collection.Find(new BsonDocument()).CountDocuments();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public long SelectCount(string collectionName, FilterDefinition<BsonDocument> filter)
+        {
+            var collection = _database.GetCollection<BsonDocument>(collectionName);
+            return collection.Find(filter).CountDocuments();
+        }
+
+        public long SelectCount(string collectionName, string field, string value)
+        {
+            var collection = _database.GetCollection<BsonDocument>(collectionName);
+            return collection.Find(Builders.FilterEq(field, value)).CountDocuments();
+        }
+
+        public long SelectCount(string collectionName, string field, ObjectId id)
+        {
+            var collection = _database.GetCollection<BsonDocument>(collectionName);
+            return collection.Find(Builders.FilterEq(field, id)).CountDocuments();
+        }
+
+        public long SelectCount<T>(string collectionName, string field, T value)
+        {
+            var collection = _database.GetCollection<BsonDocument>(collectionName);
+            return collection.Find(Builders.FilterEq<T>(field, value)).CountDocuments;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
